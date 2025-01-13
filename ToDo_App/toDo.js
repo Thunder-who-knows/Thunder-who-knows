c3cd103c63e7fa388b3d640bf6d2bd58decdf92e
Tuchar Chandra Das <dastuchar@gmail.com>
2025-01-13 23:50:31 +0530

    Added a no tasks message

--- ToDo_App/toDo.js
+++ ToDo_App/toDo.js
@@ -1,18 +1,18 @@
 const listContainer = document.querySelector('ul')
 const userInputElement = document.getElementById('user-input')
 const addButtonElement = document.getElementById('add-button')
+const noTasksElement = document.getElementById('no-tasks')
 
 
 function toggleChecked(event) {
-    let liElement = event.target
-    console.dir(liElement)
-    let tickElement = event.target.children[0]
-    if (liElement.class === 'checked')
+    let liElement = event.target.parentElement
+    let tickElement = liElement.children[0]
+    if (liElement.className === 'checked')
     {
-        liElement.class = ''
+        liElement.className = ''
         tickElement.textContent = ''
     } else {
-        liElement.class = 'checked'
+        liElement.className = 'checked'
         tickElement.textContent = ''
     }
 }
@@ -26,14 +26,18 @@ function addItem(event){
     if (newItemData.length === 0){
         alert("Enter Something")
         return
-
-        // Code to Create an Unchecked li element    
     }
+
+    // Hide the No tasks message
+    noTasksElement.style.display ='none'
+
+    // Code to Create an Unchecked li element    
     let newItem = document.createElement('li')
     newItem.innerHTML = createItemHTML(newItemData)
-
+    userInputElement.value = ''
     listContainer.append(newItem)
     newItem.addEventListener('click',toggleChecked)
+
 }
 
 addButtonElement.addEventListener('click',addItem)
